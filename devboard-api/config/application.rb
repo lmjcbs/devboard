33e42967995936fd52e8d75171f587f3b07ffa9c
lmjcbs <hello@liamjacobs.dev>
2020-02-11 19:09:49 +0000

    enable CORS

--- devboard-api/config/application.rb
+++ devboard-api/config/application.rb
@@ -24,6 +24,14 @@ module DevboardApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Rack CORS for handling Cross-Origin Resource Sharing
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+          origins '*'
+          resource '*', headers: :any, methods: [:get, :post]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
