dc58734454217c68a0d8cba7fa5bfb79bf7a7434
lmjcbs <hello@liamjacobs.dev>
2020-02-12 14:28:14 +0000

    add controller actions and private methods

--- devboard-api/app/controllers/positions_controller.rb
+++ devboard-api/app/controllers/positions_controller.rb
@@ -1,3 +1,50 @@
 class PositionsController < ApplicationController
-  
+  before_action :set_position, only: [:show, :update, :destroy]
+
+  # GET /positions
+  def index
+    render json: Position.all
+  end
+
+  # POST /positions
+  def create
+    @position = Position.new(position_params)
+    # @position.set_location(params[:position][:city])
+    # @position.set_technology(params[:position][:technology])
+    # @position.set_category(params[:position][:category])
+    if @position.save
+      render json: @position, status: :created
+    else
+      render json: @position.errors, status: :unprocessable_entity
+    end
+  end
+
+  # GET /position/:id
+  def show
+    render json: @position
+  end
+
+  # PATCH /position/:id
+  def update
+    if @position.update(position_params)
+      render json: @position
+    else
+      render json: @position.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /position/:id
+  def destroy
+    @position.destroy
+  end
+
+  private
+
+  def set_position
+    @position = position.find(params[:id])
+  end
+
+  def position_params
+    params.require(:position).permit(:title, :company, :description, :category, :location, :technology, :salary_gbp, :experience_required)
+  end
 end
