cfe563361ac691bdaf6d804b5ac0255e75b834fb
lmjcbs <hello@liamjacobs.dev>
2020-02-12 10:56:46 +0000

    add index and show actions

--- devboard-api/app/controllers/categories_controller.rb
+++ devboard-api/app/controllers/categories_controller.rb
@@ -1,3 +1,25 @@
 class CategoriesController < ApplicationController
-  
+  before_action :set_category, only: [:show, :update, :destroy]
+
+  # GET /categories
+  def index
+    @categories = Category.all
+
+    render json: @categories
+  end
+
+  # GET /category/:id
+  def show
+    render json: @category
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def location_params
+    params.require(:category).permit(:name)
+  end
 end
