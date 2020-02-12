debd500638800abef36ad0c9dee051f9ff8353d4
lmjcbs <hello@liamjacobs.dev>
2020-02-12 14:33:38 +0000

    cleaned up unused routes

--- devboard-api/config/routes.rb
+++ devboard-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :locations
-  resources :positions
-  resources :technologies 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :categories, only: [:index, :show]
+  resources :locations, only: [:index, :show]
+  resources :technologies, only: [:index, :show]
+  resources :positions
 end
