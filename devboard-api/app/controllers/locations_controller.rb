a8c2bfbca07c9554e7fee256e7ff6f6e6e7bb010
lmjcbs <hello@liamjacobs.dev>
2020-02-12 11:07:06 +0000

    add controller actions & private methods

--- devboard-api/app/controllers/locations_controller.rb
+++ devboard-api/app/controllers/locations_controller.rb
@@ -1,3 +1,50 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: [:show, :update, :destroy]
 
+  # GET /locations
+  def index
+    @locations = Location.all
+
+    render json: @locations
+  end
+
+  # POST /locations
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      render json: @location, status: :created
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
+  end
+
+  # GET /location/:id
+  def show
+    render json: @location
+  end
+
+  # PATCH /location/:id
+  def update
+    if @location.update(location_params)
+      render json: @location
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /locations/:id
+  def destroy
+    @location.destroy
+  end
+
+  private
+
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:city)
+  end
 end
