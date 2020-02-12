82e2bc0bb1fd5ff7faf764da4b7626ff384e35b6
lmjcbs <hello@liamjacobs.dev>
2020-02-12 14:27:49 +0000

    refactored removing unused actions

--- devboard-api/app/controllers/categories_controller.rb
+++ devboard-api/app/controllers/categories_controller.rb
@@ -1,25 +1,11 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :update, :destroy]
-
   # GET /categories
   def index
-    @categories = Category.all
-
-    render json: @categories
+    render json: Category.all
   end
 
   # GET /category/:id
   def show
-    render json: @category
-  end
-
-  private
-
-  def set_category
-    @category = Category.find(params[:id])
-  end
-
-  def location_params
-    params.require(:category).permit(:name)
+    render json: Category.find(params[:id])
   end
 end
--- devboard-api/app/controllers/technologies_controller.rb
+++ devboard-api/app/controllers/technologies_controller.rb
@@ -1,50 +1,11 @@
 class TechnologiesController < ApplicationController
-  before_action :set_technology, only: [:show, :update, :destroy]
-
   # GET /technologies
   def index
-    @technologies = Technology.all
-
-    render json: @technologies
-  end
-
-  # POST /technologies
-  def create
-    @technology = Technology.new(location_params)
-
-    if @technology.save
-      render json: @technology, status: :created
-    else
-      render json: @technology.errors, status: :unprocessable_entity
-    end
+    render json: Technology.all
   end
 
   # GET /technology/:id
   def show
-    render json: @technology
-  end
-
-  # PATCH /technology/:id
-  def update
-    if @technology.update(technology_params)
-      render json: @technology
-    else
-      render json: @technology.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /technology/:id
-  def destroy
-    @technology.destroy
-  end
-
-  private
-
-  def set_technology
-    @technology = Technology.find(params[:id])
-  end
-
-  def location_params
-    params.require(@technology).permit(:name)
+    render json: Technology.find(params[:id])
   end
 end
