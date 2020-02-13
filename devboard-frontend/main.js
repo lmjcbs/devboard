e37f235d13655ed47d25fbca3ab7557863b4bc9f
lmjcbs <hello@liamjacobs.dev>
2020-02-13 17:33:49 +0000

    refactored position class

--- devboard-frontend/main.js
+++ devboard-frontend/main.js
@@ -1,39 +1,17 @@
 const BASE_URL = 'http://localhost:3000'
-const container = document.querySelector('#positions-container');
-
-class Position {
-  constructor(pos) {
-    this.id = pos.id
-    this.title = pos.title
-    this.company = pos.company
-    this.description = pos.description
-    this.salaryGBP = pos.salaryGBP
-    this.experienceRequired = pos.experienceRequired
-    this.location = pos.location
-    this.category = pos.category
-    this.technology = pos.technology
-  }
-
-  renderPosition() {
-    return `
-    <div id="position-${this.id}">
-      <h3>${this.title} at ${this.company}</h3>
-      <p>${this.location}</p>
-      <p>${this.category} |  ${this.technology}</p>
-      <p>Salary: £${this.salaryGBP}</p>
-      <p>Experience Required: ${this.experienceRequired}</p>
-      <p>${this.description}</p>
-    </div>
-    `
-  }
-}
+const containerTitle = () => document.querySelector('#container-title')
+const container = () => document.querySelector('#positionsContainer')
 
 const renderPositions = () => {
   fetch(`${BASE_URL}/positions`)
   .then(resp => resp.json())
   .then(positions => {
+    clearContainer()
+    // const containerTitle = document.querySelector('#container-title')
+    containerTitle().textContent = 'All Positions'
     const positionsArray = positions.map(position => new Position(position))
-    positionsArray.forEach(position => container.innerHTML += position.renderPosition())
+    // const container = document.querySelector('#positionsContainer')
+    positionsArray.forEach(position => container().innerHTML += position.renderPosition())
   })
 }
 
@@ -41,7 +19,12 @@ const renderLocations = () => {
   fetch(`${BASE_URL}/locations`)
   .then(resp => resp.json())
   .then(locations => {
-    console.log(locations)
+    clearContainer()
+    // const containerTitle = document.querySelector('#container-title')
+    containerTitle().textContent = 'All Locations'
+    const locationsArray = locations.map(location => new Location(location))
+    // const container = document.querySelector('#positionsContainer')
+    locationsArray.forEach(location => container().innerHTML += location.renderLocation())
   })
 }
 
@@ -49,7 +32,8 @@ const renderCategories = () => {
   fetch(`${BASE_URL}/categories`)
   .then(resp => resp.json())
   .then(categories => {
-
+    const categoriesArray = categories.map(category => new Category(category))
+    console.log(categoriesArray)
   })
 }
 
@@ -61,6 +45,13 @@ const renderTechnlogies = () => {
   })
 }
 
+const clearContainer = () => {
+  document.querySelector('#positionsContainer').innerHTML = ''
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  renderPositions()
+  // console.log(container)
+  // renderPositions()
+  renderLocations()
+
 });
