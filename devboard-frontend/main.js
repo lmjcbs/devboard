db89191471b1de60d9e7dda2c26aa710b5be8ae2
lmjcbs <hello@liamjacobs.dev>
2020-02-13 17:49:51 +0000

    refactored container globals to be functions

--- devboard-frontend/main.js
+++ devboard-frontend/main.js
@@ -1,16 +1,14 @@
 const BASE_URL = 'http://localhost:3000'
 const containerTitle = () => document.querySelector('#container-title')
-const container = () => document.querySelector('#positionsContainer')
+const container = () => document.querySelector('#positions-container')
 
 const renderPositions = () => {
   fetch(`${BASE_URL}/positions`)
   .then(resp => resp.json())
   .then(positions => {
     clearContainer()
-    // const containerTitle = document.querySelector('#container-title')
     containerTitle().textContent = 'All Positions'
     const positionsArray = positions.map(position => new Position(position))
-    // const container = document.querySelector('#positionsContainer')
     positionsArray.forEach(position => container().innerHTML += position.renderPosition())
   })
 }
@@ -20,10 +18,8 @@ const renderLocations = () => {
   .then(resp => resp.json())
   .then(locations => {
     clearContainer()
-    // const containerTitle = document.querySelector('#container-title')
     containerTitle().textContent = 'All Locations'
     const locationsArray = locations.map(location => new Location(location))
-    // const container = document.querySelector('#positionsContainer')
     locationsArray.forEach(location => container().innerHTML += location.renderLocation())
   })
 }
