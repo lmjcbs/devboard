6dd0bf9bd152972318a7a80e2f44b023885d42a5
lmjcbs <hello@liamjacobs.dev>
2020-02-13 20:05:16 +0000

    add position form

--- devboard-frontend/main.js
+++ devboard-frontend/main.js
@@ -32,7 +32,7 @@ const renderCategories = () => {
   })
 }
 
-const renderTechnlogies = () => {
+const renderTechnologies = () => {
   fetch(`${BASE_URL}/technologies`)
   .then(resp => resp.json())
   .then(technologies => {
@@ -40,6 +40,40 @@ const renderTechnlogies = () => {
     container().innerHTML = technologiesArray.reduce((all, tec) => all += tec.renderTechnology(),'')
     containerTitle().textContent = 'All Technologies'
   })
+}
+
+const renderPositionForm = () => {
+  containerTitle().textContent = 'Advertise your own position'
+  container().innerHTML = `
+    <form onsubmit="createPosition();return false;>
+
+      <label for="title">Title</label>
+      <input type="text" id="title" name="title" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="company">Company</label>
+      <input type="text" id="company" name="company" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="location">Location</label>
+      <input type="text" id="location" name="location" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+      
+      <label for="category">Category</label>
+      <input type="text" id="category" name="category" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="technology">Technology</label>
+      <input type="text" id="technology" name="technology" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="salary_gbp">Salary</label>
+      <input type="text" id="salary-gbp" name="salary-gbp" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="experience_required">Experience Required</label>
+      <input type="text" id="experience_required" name="experience_required" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-1 px-2 pl-10 appearance-none leading-normal"><br>
+
+      <label for="description">Description</label>
+      <textarea id="description" name="description" class="w-full bg-gray-100 text-sm text-gray-800 transition border focus:outline-none focus:border-purple-500 rounded py-8 px-2 pl-10 appearance-none leading-normal"></textarea><br>
+
+      <input type="submit" value="Submit">
+    </form>
+  `
 }
 
 document.addEventListener("DOMContentLoaded", () => {
