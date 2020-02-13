9b27133d93fddd77ede73866e995e802947d3bbd
lmjcbs <hello@liamjacobs.dev>
2020-02-13 21:22:00 +0000

    fixed custom setters

--- devboard-api/app/models/position.rb
+++ devboard-api/app/models/position.rb
@@ -2,4 +2,16 @@ class Position < ApplicationRecord
   belongs_to :location
   belongs_to :category
   belongs_to :technology
+
+  def category=(category)
+    write_attribute(:category_id, Category.where(:name => category).first_or_create.id)
+  end
+
+  def location=(location)
+    write_attribute(:location_id, Location.where(:city => location).first_or_create.id)
+  end
+
+  def technology=(technology)
+    write_attribute(:technology_id, Technology.where(:name => technology).first_or_create.id)
+  end
 end
